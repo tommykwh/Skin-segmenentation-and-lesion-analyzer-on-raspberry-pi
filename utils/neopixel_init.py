986fe1781b3becaca97abc71262aa31966dfea06
Tommy Ho <kuanweih@uw.edu>
2020-05-02 22:26:58 -0700

    neopixel fix

--- utils/neopixel_init.py
+++ utils/neopixel_init.py
@@ -1,5 +1,4 @@
 import board
 import neopixel
-pixels = neopixel.NeoPixel(board.D21, 12)
-# pixels[0] = (255, 0, 0)
-pixels.fill((5, 5, 5))
+pixels = neopixel.NeoPixel(board.D18, 12)
+pixels.fill((6, 9, 5))

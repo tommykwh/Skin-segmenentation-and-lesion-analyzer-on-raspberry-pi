da5f0dfae887bb26a35699565a4b4684924bde4c
Thanatine <firewings89504@gmail.com>
2020-05-28 13:58:41 -0700

    5/28 new seg integrrated

--- demo/demo.py
+++ demo/demo.py
@@ -3,6 +3,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Convolution2D, MaxPooling2D, UpSampling2D, Concatenate, Dense
+from tensorflow_examples.models.pix2pix import pix2pix
 import os
 import matplotlib.pyplot as plt
 from PIL import Image
@@ -11,88 +12,92 @@ import cv2
 
 # %matplotlib inline
 
-raw_img = Image.open('test_image.jpg')
-
+raw_img = Image.open('../assets/test_image.jpg')
 raw_img = np.array(raw_img)
 img = tf.keras.applications.mobilenet.preprocess_input(np.array(cv2.resize(raw_img, (224, 224))))
 # plt.imshow(img)
 
-filepath = 'mobilenetv2_model.h5'
+filepath = '../models/mobilenetv2_model.h5'
 IMG_SHAPE = (224, 224, 3)
 mobilev2 = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE, weights='imagenet')
 x = mobilev2.layers[-2].output
 predictions = Dense(7, activation='softmax')(x)
-model = Model(inputs=mobilev2.input, outputs=predictions)
-model.load_weights(filepath)
+recg_model = Model(inputs=mobilev2.input, outputs=predictions)
+recg_model.load_weights(filepath)
 
-
-result = model.predict(np.array([img]))
+result = recg_model.predict(np.array([img]))
 print(np.shape(result))
 print(result)
 print(np.argmax(result))
 
-img_rows = 192
-img_cols = 240
 
-def get_unet():
-    concat_axis = 3
-    
-    inputs = Input(shape=[img_rows, img_cols, 1])
-    conv1 = Convolution2D(32, (3, 3), activation='relu', padding='same')(inputs)
-    conv1 = Convolution2D(32, (3, 3), activation='relu', padding='same')(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+def unet_model(output_channels):
+    # Create the base model from the pre-trained model MobileNet V2
+    # mobilev2 = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE, weights='imagenet', include_top=False)
+    # x = mobilev2.layers[-2].output
+    # predictions = Dense(7, activation='softmax')(x)
+    # base_model = Model(inputs=mobilev2.input, outputs=predictions)
+    # base_model.load_weights(filepath)
 
-    conv2 = Convolution2D(64, (3, 3), activation='relu', padding='same')(pool1)
-    conv2 = Convolution2D(64, (3, 3), activation='relu', padding='same')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    # Use the activations of these layers
+    layer_names = [
+        'block_1_expand_relu',   # 64x64
+        'block_3_expand_relu',   # 32x32
+        'block_6_expand_relu',   # 16x16
+        'block_13_expand_relu',  # 8x8
+        'block_16_project',      # 4x4
+    ]
+    layers = [recg_model.get_layer(name).output for name in layer_names]
 
-    conv3 = Convolution2D(128, (3, 3), activation='relu', padding='same')(pool2)
-    conv3 = Convolution2D(128, (3, 3), activation='relu', padding='same')(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    # Create the feature extraction model
+    down_stack = tf.keras.Model(inputs=recg_model.input, outputs=layers)
+    down_stack.trainable = False
 
-    conv4 = Convolution2D(256, (3, 3), activation='relu', padding='same')(pool3)
-    conv4 = Convolution2D(256, (3, 3), activation='relu', padding='same')(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+    up_stack = [
+        pix2pix.upsample(512, 3),  # 4x4 -> 8x8
+        pix2pix.upsample(256, 3),  # 8x8 -> 16x16
+        pix2pix.upsample(128, 3),  # 16x16 -> 32x32
+        pix2pix.upsample(64, 3),   # 32x32 -> 64x64
+    ]
 
-    conv5 = Convolution2D(512, (3, 3), activation='relu', padding='same')(pool4)
-    conv5 = Convolution2D(512, (3, 3), activation='relu', padding='same')(conv5)
+    inputs = tf.keras.layers.Input(shape=IMG_SHAPE)
+    x = inputs
 
-#     up6 = merge([UpSampling2D(size=(2, 2))(conv5), conv4], mode='concat', concat_axis=1)
-    up6 = Concatenate(axis=concat_axis)([UpSampling2D(size=(2, 2))(conv5), conv4])
-    conv6 = Convolution2D(256, (3, 3), activation='relu', padding='same')(up6)
-    conv6 = Convolution2D(256, (3, 3), activation='relu', padding='same')(conv6)
+    # Downsampling through the model
+    skips = down_stack(x)
+    x = skips[-1]
+    skips = reversed(skips[:-1])
 
-#     up7 = merge([UpSampling2D(size=(2, 2))(conv6), conv3], mode='concat', concat_axis=1)
-    up7 = Concatenate(axis=concat_axis)([UpSampling2D(size=(2, 2))(conv6), conv3])
-    conv7 = Convolution2D(128, (3, 3), activation='relu', padding='same')(up7)
-    conv7 = Convolution2D(128, (3, 3), activation='relu', padding='same')(conv7)
+    # Upsampling and establishing the skip connections
+    for up, skip in zip(up_stack, skips):
+        x = up(x)
+        concat = tf.keras.layers.Concatenate()
+        x = concat([x, skip])
 
-#     up8 = merge([UpSampling2D(size=(2, 2))(conv7), conv2], mode='concat', concat_axis=1)
-    up8 = Concatenate(axis=concat_axis)([UpSampling2D(size=(2, 2))(conv7), conv2])
-    conv8 = Convolution2D(64, (3, 3), activation='relu', padding='same')(up8)
-    conv8 = Convolution2D(64, (3, 3), activation='relu', padding='same')(conv8)
+    # This is the last layer of the model
+    last = tf.keras.layers.Conv2DTranspose(
+        filters=1,
+        kernel_size=3,
+        strides=2,
+        padding='same')  #64x64 -> 128x128
 
-#     up9 = merge([UpSampling2D(size=(2, 2))(conv8), conv1], mode='concat', concat_axis=1)
-    up9 = Concatenate(axis=concat_axis)([UpSampling2D(size=(2, 2))(conv8), conv1])
-    conv9 = Convolution2D(32, (3, 3), activation='relu', padding='same')(up9)
-    conv9 = Convolution2D(32, (3, 3), activation='relu', padding='same')(conv9)
+    x = last(x)
 
-    conv10 = Convolution2D(1, (1, 1), activation='sigmoid')(conv9)
+    return tf.keras.Model(inputs=inputs, outputs=x)
 
-    model = Model(inputs=inputs, outputs=conv10)
-
-    return model
-
-
-filepath = 'unet.hdf5'
-seg_model = get_unet()
+OUTPUT_CHANNELS = 1
+filepath = '../models/tf_unet.hdf5'
+seg_model = unet_model(OUTPUT_CHANNELS)
 seg_model.load_weights(filepath)
 
 def preprocess(imgs):
-    gray_image = cv2.cvtColor(raw_img, cv2.COLOR_BGR2GRAY)
-    imgs_p = np.ndarray((imgs.shape[0], img_rows, img_cols, 1), dtype=np.uint8)
-    resized_image = cv2.resize(gray_image, (img_cols, img_rows), interpolation=cv2.INTER_CUBIC)
-    imgs_p = resized_image.reshape(resized_image.shape + (1,))
+    imgs_p = np.ndarray((imgs.shape[0], IMG_SHAPE[0], IMG_SHAPE[1], IMG_SHAPE[2]), dtype=np.uint8)
+    for i, img in enumerate(imgs):
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype(np.float32)
+        img = cv2.resize(img, (IMG_SHAPE[0], IMG_SHAPE[1]), interpolation=cv2.INTER_CUBIC)
+        img = np.array(img)
+        imgs_p[i] = img
+
     imgs_p = imgs_p.astype('float32')
     mean = np.mean(imgs_p)
     std = np.std(imgs_p)
@@ -101,19 +106,19 @@ def preprocess(imgs):
 
     return imgs_p
 
-test = preprocess(raw_img)
+test = preprocess(np.array([raw_img]))
+prediction = seg_model.predict(test)
 
-prediction = seg_model.predict(np.array([test]))
-
-img = cv2.resize(raw_img, (240, 192))
+img = cv2.resize(raw_img, (IMG_SHAPE[0], IMG_SHAPE[1]))
 plt.imshow(img)
 plt.show()
 
 mask = cv2.inRange(cv2.cvtColor(prediction[0], cv2.COLOR_GRAY2RGB),
-                   (0.9, 0.9, 0.9),
+                   (0.6, 0.6, 0.6),
                    (1, 1, 1))
+
 # Create a blank 300x300 black image
-red = np.zeros((192, 240, 3), np.uint8)
+red = np.zeros((IMG_SHAPE[0], IMG_SHAPE[1], 3), np.uint8)
 # Fill image with red color(set each pixel to red)
 red[:] = (0, 30, 0)
 
